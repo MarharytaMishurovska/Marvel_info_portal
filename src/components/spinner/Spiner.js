045ad8355a31cf2ccd2c274dd4a7d361df650c2e
Marharyta Mishurovska <marharyta.mishurovska@gmail.com>
2022-03-04 18:13:31 +0200

    Create Marvel info-portal

---
+++ src/components/spinner/Spiner.js
@@ -0,0 +1,22 @@
+import './spinner.scss';
+
+import {Component} from 'react';
+
+class Spinner extends Component {
+  render() {
+    return(
+      <div className='spinner'>
+        <div className="loadingio-spinner-ripple-77miozdovik">
+          <div className="ldio-b7lcyayfer8">
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Spinner;
+
+
